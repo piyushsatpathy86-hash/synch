dcd1611e02e01bc8e46b45441b1fbfc4e362988c
PiyushSatpathy <piyushsatpathy86@gmail.com>
2026-07-11 13:02:54 +0530

    Add About page, remove duplicate src folder

--- synch/src/App.jsx
+++ synch/src/App.jsx
@@ -14,6 +14,7 @@ import CreateProject from "./pages/CreateProject";
 import JoinProject from "./pages/JoinProject";
 import Project from "./pages/Project";
 import MyProjects from "./pages/MyProjects";
+import About from "./pages/About";
 
 export default function App() {
   return (
@@ -47,6 +48,7 @@ export default function App() {
 <Route path="/join-project" element={<ProtectedRoute><JoinProject /></ProtectedRoute>} />
 <Route path="/project/:id" element={<ProtectedRoute><Project /></ProtectedRoute>} />
 <Route path="/my-projects" element={<ProtectedRoute><MyProjects /></ProtectedRoute>} />
+<Route path="/about" element={<About />} />
       </Routes>
     </>
   );
